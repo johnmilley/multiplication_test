0eb60d9a0470be3c2f0177f9bd5cfa7bb86cc8ca
johnmilley <mushmushthemarvelousmonkey@hotmail.com>
2024-02-01 14:23:35 -0330

    v0.2 keypad with results screen

--- timestables.js
+++ timestables.js
@@ -1,12 +1,22 @@
 // Multiplication Practice
-// version 0.1
-// it works.
+// version 0.2
+// What's new?
+//  - keypad for mobile
+//  - 10 random q from array
+//  - results board
 
 
 // globals
+let allPossibleEquations = []
 let equations = []
 let retries = []
 let equation = []
+
+// test global for keypad
+let keyPadInput = ""
+
+// stats globals
+let totalCorrect = 0
 
 // timer globals
 let time = 15
@@ -15,26 +25,55 @@ let timer = ""  // for global interval timer
 // GENERATE EQUATIONS
 for (let i = 3; i <= 5; i++) {
     for (let j = 2; j <= 9; j++) {
-        equations.push([i, j])
+        allPossibleEquations.push([i, j])
     }
 }
 
+// change totalQuestions to control how many q user sees
+totalQuestions = 1
+
+for (let i = 1; i <= totalQuestions; i++) {
+    random_index = Math.floor(Math.random() * allPossibleEquations.length)
+
+    current_equation = allPossibleEquations[random_index]
+
+    equations.push(current_equation)
+
+    // remove item from array
+    allPossibleEquations.splice(random_index, 1)
+}
+/***************/
+
 /*  GRAB HTML ELEMENTS */
 
-const equationArea = document.querySelector('main')
-const questionsRemaining = document.querySelector('#questions-remaining')
+const equationArea = document.querySelector('#equation-area')
+
 const leftOperand = document.querySelector('#left')
 const rightOperand = document.querySelector('#right')
-const playButton = document.querySelector('#play');
-const timerElement = document.querySelector('#time-remaining')
 const answerInput = document.querySelector("#answer")
-const instructionsElement = document.querySelector('#instructions')
+
+const questionsRemaining = document.querySelector('#questions-remaining')
+
+const playButton = document.querySelector('#play');
+
+const timerArea = document.querySelector('#timer-area')
+const timerElement = document.querySelector('#time-remaining')
+
+const keyPadArea = document.querySelector('#keypad-area')
+
+// const instructionsElement = document.querySelector('#instructions')
+
+// results
+const resultsArea = document.querySelector('#results')
+
+const numberCorrectElement = document.querySelector('#number-correct')
+const totalNumberOfQuestionsElement = document.querySelector('#total-number-of-questions')
+const percentElement = document.querySelector('#percent')
+const letterGradeElement = document.querySelector('#letter-grade')
+
+
 
 /******************/
-
-
-// newQuestion() // generate starting q
-// setTimer()    // begin timer
 
 
 /* CHECK ANSWER by pressing Enter or clicking button */
@@ -56,16 +95,14 @@ function checkAnswer(e) {
     // compare answer to input
     if (answerInput.value == answer) {
         equationArea.style.backgroundColor = "lightgreen";
+        totalCorrect++
     } else {
         equationArea.style.backgroundColor = "#da4a4a";
         retries.push(equation)
         console.log(retries)
     }
     
-    // resetTimer
-    // FIX timer not restarting when time runs out... think about this
-    // diagram it.
-    // setTimer()
+    input = ""
 
     // delay for new question
     setTimeout(newQuestion, 1500)
@@ -82,23 +119,49 @@ function setTimer(seconds=15) {
 
 function newQuestion() {
 
-    //todo logic for retries
-    // if (equations.length <= 0) {
+    // first, check if there are new questions. if not, present results (for now)
 
-    //} else if (retries > 0) {
-        // take questions from retries
-    //} else {
-        // display the answers to missed questions
-    //}
-    //
-    //
+    // move to function
+    if (equations <= 0) {
+        equationArea.classList.toggle('hidden')
+        timerArea.classList.toggle('hidden')
+        questionsRemaining.classList.toggle('hidden')
+        keyPadArea.classList.toggle('hidden')
 
-    //todo RESET timer
-    //  elsewhere - if timer reaches 0, add q to retries and run newQuestion()
+        numberCorrectElement.innerText = totalCorrect
+        totalNumberOfQuestionsElement.innerText = totalQuestions
+
+        let percent = ((totalCorrect / totalQuestions) * 100).toFixed(0)
+
+        percentElement.innerText = `${percent}%`
+        
+        let letterGrade = ""
+
+        if (percent >= 80) {
+            letterGrade = 'A'
+        } else if (percent >= 70) {
+            letterGrade = 'B'
+        } else if (percent >= 60) {
+            letterGrade = 'C'
+        } else if (percent >= 50) {
+            letterGrade = 'D'
+        } else {
+            letterGrade = 'F'
+        }
+
+        letterGradeElement.innerText = letterGrade
+
+        resultsArea.classList.toggle('hidden')
+        
+        clearInterval(timer)
+
+        return
+    }
 
     // RESET THINGS
     equationArea.style.backgroundColor = "white";
     questionsRemaining.innerText = `Questions remaining: ${equations.length}`
+    keyPadInput = ""
     answerInput.value = ""
 
 
@@ -141,20 +204,73 @@ function countdown() {
 }
 
 
-// play/pause
-
+// START the practice
 
 playButton.addEventListener('click', () => {
     playButton.disabled = true
     timerElement.classList.toggle('hidden')
     playButton.classList.toggle('hidden')
-    instructionsElement.classList.toggle('hidden')
+    keyPadArea.classList.remove('hidden')
     answerInput.focus()
     newQuestion()
 })
+
+
+// KEYPAD Testing
+
+const numberKeys = document.querySelectorAll('[data-key]')
+const backspaceKey = document.querySelector('#backspace-key')
+const enterKey = document.querySelector('#enter-key')
+
+numberKeys.forEach(key => key.addEventListener('click', (e) => {
+    console.log(e);
+    // e.preventDefault() // not working for 'enter key'
+    if (e.clientX !== 0) {  // means 'enter' was used to 'click' btn ... not great, but it works
+        keyPadInput += e.target.getAttribute('data-key')
+        console.log(keyPadInput)
+        if (!answerInput.disabled) {
+            answerInput.value = keyPadInput
+            answerInput.focus()
+        }
+    }
+}))
+
+backspaceKey.addEventListener('click', () => {
+    if (keyPadInput.length > 0) {
+        keyPadInput = keyPadInput.slice(0, keyPadInput.length - 1)
+        console.log(keyPadInput);
+    }
+    if (!answerInput.disabled) {
+        answerInput.value = keyPadInput
+        answerInput.focus()
+    }
+})
+
+enterKey.addEventListener('click', (e) => {
+    if (e.clientX !== 0) {  // means 'enter' was used to 'click' btn ... not great, but it works
+        checkAnswer()
+    }
+})
+
+// END KEYPAD functions
 
 
 
 // LEFTOVERS
 
 // equations = equations.splice(i, 1) // remove equation
+
+
+//todo logic for retries
+    // if (equations.length <= 0) {
+
+    //} else if (retries > 0) {
+        // take questions from retries
+    //} else {
+        // display the answers to missed questions
+    //}
+    //
+    //
+
+    //todo RESET timer
+    //  elsewhere - if timer reaches 0, add q to retries and run newQuestion()
